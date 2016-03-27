22f24c5316c18e112f3386f578d801e0b01891c2
mondaugen <nicholas.esterer@gmail.com>
2016-03-26 20:00:16 -0400

    Added comment on Dirichlet kernel.

--- dk.m
+++ dk.m
@@ -2,6 +2,7 @@ function [y]=dk(theta,N)
 % [y]=dk(theta,N)
 % Compute the dirichlet kernel of order N.
 % theta is in radians
+% Multiplied by complex exponential to centre around N/2 in time-domain
 y=exp(-j*N*theta/2).*sin((N/2+1/2)*theta)./sin(theta/2);
 % Remove NaNs because function is defined for theta=0
 y(find(theta==0))=N;
