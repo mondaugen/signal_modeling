a7811718afca795ce55d3159a539cd3472efb017
mondaugen <nicholas.esterer@gmail.com>
2016-05-25 09:57:17 -0400

    Fixing harm_sines to be scriptable. Still not working.

--- harm_sines.m
+++ harm_sines.m
@@ -4,10 +4,6 @@ function [x,opt]=harm_sines(opt=struct())
 sp.Fs=16000;
 % Length of signal
 sp.N=sp.Fs*1.5;
-% sample indices
-n=(0:(sp.N-1));
-n=n(:);
-t=n/sp.Fs;
 % Fundamental
 sp.f0=440*2^((67-69)/12);
 % Number of harmonics
@@ -15,14 +11,8 @@ sp.K=10;
 % Inharmonicity coefficient
 %sp.B=exp(2.54*log(sp.f0)-24.6); % For now, the just noticeable inharmonic coefficient
 sp.B=0.001;
-% Harmonic numbers
-k=1:sp.K;
-% Harmonic numbers adjusted for inharmonicity
-sp.k_B=k.*sqrt(1+sp.B*k.^2);
 % Amplitude based on harmonic number
 sp.A_k_60=20; % The harmonic number that is 60dB lower than the first
-a_k_60=log(10^(-3))/log(10)/sp.A_k_60;
-sp.A_k=exp(a_k_60*k); % use original harmonic numbers
 % Initial phase
 sp.phi=0;
 % Initial FM phase
@@ -30,8 +20,6 @@ sp.phi_fm=0;
 % Time in seconds until amplitude of partial has dropped by 60dB
 %sp.T_60=1;
 sp.T_60=.75;
-% amplitude coefficient
-a_60=log(10^(-3))/log(10)/sp.T_60;
 % Time in seconds to when attack function reaches maximum
 sp.T_max=0.1;
 sp.A_method='FOF';
@@ -44,22 +32,35 @@ for fn=fieldnames(sp)'
     end
 end
 
-switch sp.A_method
+% amplitude coefficient
+a_60=log(10^(-3))/log(10)/opt.T_60;
+% Harmonic numbers
+k=1:opt.K;
+% Harmonic numbers adjusted for inharmonicity
+opt.k_B=k.*sqrt(1+opt.B*k.^2);
+a_k_60=log(10^(-3))/log(10)/opt.A_k_60;
+opt.A_k=exp(a_k_60*k); % use original harmonic numbers
+% sample indices
+n=(0:(opt.N-1));
+n=n(:);
+t=n/opt.Fs;
+
+switch opt.A_method
 case 'FOF'
     % using FOF functions
-    A=exp(a_60*t)*sp.A_k.*(cos(t/sp.T_max*pi/2-pi/2).^2.*(t<=sp.T_max)+(t>sp.T_max));
+    A=exp(a_60*t)*opt.A_k.*(cos(t/opt.T_max*pi/2-pi/2).^2.*(t<=opt.T_max)+(t>opt.T_max));
 case 'AR1'
     % Using first order AR model
     C_max=0.99;
-    n_max=sp.Fs*sp.T_max;
+    n_max=opt.Fs*opt.T_max;
     a_T_max=-(1-C_max)^(1/(n_max+1));
-    A=filter([1+a_T_max],[1 a_T_max],exp(a_60*t)*sp.A_k,[],1);
+    A=filter([1+a_T_max],[1 a_T_max],exp(a_60*t)*opt.A_k,[],1);
 otherwise
     error('Bad A_method');
 end
 % Amplitude coefficient of FM
-sp.A_fm=sp.f0*2^(1/12)-sp.f0;
+opt.A_fm=opt.f0*2^(1/12)-opt.f0;
 % Frequency coefficient of FM
-sp.f_fm=2;
-x=A.*cos(2*pi*(sp.f0*t-sp.A_fm/(2*pi*sp.f_fm)*cos(2*pi*sp.f_fm*t+sp.phi_fm))*sp.k_B+sp.phi);
+opt.f_fm=2;
+x=A.*cos(2*pi*(opt.f0*t-opt.A_fm/(2*pi*opt.f_fm)*cos(2*pi*opt.f_fm*t+opt.phi_fm))*opt.k_B+opt.phi);
 x=sum(x,2);
---
+++ harm_sines_script.m
@@ -0,0 +1,44 @@
+N_ARGS=11;
+argv=argv();
+if length(argv) != N_ARGS
+    fprintf(stderr,
+    ['Arguments are:\n'...
+    'sample-rate\n'...
+    'length-samples\n'...
+    'fundamental-Hz\n'...
+    'number-harmonics\n'...
+    'inharmonicity-coefficient\n'...
+    '60dB-harmonic-number\n'...
+    'initial-phase\n'...
+    'initial-fm-phase\n'...
+    't-60dB\n'...
+    'attack-max-time\n'...
+    'attack-method {FOF,AR1}\n']);
+    exit(-1);
+end
+
+% Sampling rate in Hz
+opt.Fs=str2num(argv{1});
+% Length of signal
+opt.N=str2num(argv{2});
+% Fundamental
+opt.f0=str2num(argv{3});
+% Number of harmonics
+opt.K=str2num(argv{4});
+% Inharmonicity coefficient
+%opt.B=exp(2.54*log(opt.f0)-24.6); % For now, the just noticeable inharmonic coefficient
+opt.B=str2num(argv{5});
+% Amplitude based on harmonic number
+opt.A_k_60=str2num(argv{6}); % The harmonic number that is 60dB lower than the first
+% Initial phase
+opt.phi=str2num(argv{7});
+% Initial FM phase
+opt.phi_fm=str2num(argv{8});
+% Time in seconds until amplitude of partial has dropped by 60dB
+%opt.T_60=1;
+opt.T_60=str2num(argv{9});
+% Time in seconds to when attack function reaches maximum
+opt.T_max=str2num(argv{10});
+opt.A_method=argv{11};
+x=harm_sines(opt)
+fwrite(stdout,x,'float64')
