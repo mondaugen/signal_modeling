18c22a791613339b0e4b63d5aad516cdaa84a07c
Nichoas Esterer <nicholas.esterer@gmail.com>
2016-07-12 19:42:47 -0400

    Trying to use viterbi algorithm to connect sources between frames
    It seems like a minimum cost path is a reasonable set of connections for 1
    source, however, the viterbi algorithm as it is now does not work for what we
    are interested in doing. We need to re-write the viterbi algorithm to make it
    work.

--- test/plot_hsrp_test_7_dat.py
+++ test/plot_hsrp_test_7_dat.py
@@ -2,11 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as sio
 import os
+import ptpath
+import ptpath_test
+from cvxopt import solvers
 
 plt.rc('text',usetex=True)
 plt.rc('font',family='serif')
 
-show_plots=True
+show_plots=False
 infilepath=os.environ['HOME']+'/Documents/development/masters_thesis/reports/plots/'
 infilepath+='hsrp_test_7.dat'
 
@@ -218,6 +221,90 @@ ax6.set_title('Source 2 before connecting paths')
 ax6.legend(lgd,('Estimated','True'))
 ax6.set_xlim(0,(h-H)/float(Fs))
 ax6.set_ylim(0,3.5)
+
+Li_Ss=list()
+Li_F_s=list()
+Li_Xs=list()
+Li_sols=list()
+Li_paths=list()
+C=list()
+C_cxn=list()
+def _node_cxn_cost(a,b,dt):
+    return ((a[0]+a[1]*dt)-b[0])**2.
+def _lp_cost_fun(a,b):
+    return _node_cxn_cost(a.value,b.value,(b.frame_num-a.frame_num)*H/float(Fs))+1.
+h=0
+sols=[]
+lgd=[[],[]]
+for i in xrange(len(datin)-1):
+    X_plt_est=datin[i]['X_plt'][0][0]
+    clr_e=datin[i]['clr_'][0][0]
+    X_s1_0=[]
+    X_s2_0=[]
+    X_s1_1=[]
+    X_s2_1=[]
+    for r,s in zip(X_plt_est,clr_e):
+        if (s=='b'):
+            X_s1_0.append(np.array([r[0],r[1]]))
+        if (s=='c'):
+            X_s2_0.append(np.array([r[0],r[1]]))
+    X_plt_est=datin[i+1]['X_plt'][0][0]
+    clr_e=datin[i+1]['clr_'][0][0]
+    for r,s in zip(X_plt_est,clr_e):
+        if (s=='b'):
+            X_s1_1.append(np.array([r[0],r[1]]))
+        if (s=='c'):
+            X_s2_1.append(np.array([r[0],r[1]]))
+
+    # LPs, find best connections
+    # 0=1-1 1=2-2 2=1-2 3=2-1
+    Ss=[dict() for _ in xrange(4)]
+    F_s=[[] for _ in xrange(4)]
+    Xs=[(X_s1_0,X_s1_1),
+        (X_s2_0,X_s2_1),
+        (X_s1_0,X_s2_1),
+        (X_s2_0,X_s1_1)]
+    sols=[]
+    paths=[]
+    for S,F,X in zip(Ss,F_s,Xs):
+        n_node=0
+        n_frame=0
+        # Build frames and graph
+        for x_ in X:
+            F.append([])
+            for k_a_ in xrange(len(x_)):
+                x__=x_[k_a_]
+                F[-1].append(n_node)
+                S[n_node]=ptpath.LPNode(value=x__,frame_num=n_frame)
+                n_node+=1
+            n_frame+=1
+        for f in F[0]:
+            S[f].out_nodes=F[1]
+        for f in F[1]:
+            S[f].in_nodes=F[0]
+        J=min(len(X_) for X_ in X)
+        # Build linear program
+        d=ptpath.g_f_2lp(S,F,J,_lp_cost_fun,{'calc_mean':0,'min_mean_dev':0})
+        # Solve LP
+        sols.append(solvers.lp(d['c'],d['G'],d['h'],d['A'],d['b']))
+        # Extract paths
+        paths.append(ptpath_test.lp_sol_extract_paths(sols[-1]['x'],S,F))
+    # add to list
+    Li_Ss.append(Ss)
+    Li_F_s.append(F_s)
+    Li_Xs.append(Xs)
+    Li_sols.append(sols)
+    Li_paths.append(paths)
+    # Add costs to cost lattice
+    C.append([sols[0]['primal objective']+sols[1]['primal objective'],
+              sols[2]['primal objective']+sols[3]['primal objective']])
+    # Add connections to connections lattice
+    C_cxn.append([((0,1),(0,1)),((0,1),(1,0))])
+
+# Now carry out viterbi algorithm to find the 2 most likely paths
+# (This could be done with the classic viterbi algorithm but it's easier to set
+# up this way)
+q_likely=ptpath_test.shortest_paths_viterbi(C,C_cxn)
 
 if (show_plots):
     plt.show()
